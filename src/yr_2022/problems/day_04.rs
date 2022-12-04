02fd29ab60deacaad52c4c809c7e56a5e8bbb640
Steven Chu <stevenc1@gmail.com>
2022-12-04 11:15:25 -0800

    Some cleanup; also setup for day 4

---
+++ src/yr_2022/problems/day_04.rs
@@ -0,0 +1,10 @@
+use crate::yr_2022::problems::utils;
+
+pub fn solution_2(input_file: &str) -> i32 {
+    let lines = utils::read_file(input_file).unwrap();
+    0
+}
+pub fn solution_1(input_file: &str) -> i32 {
+    let lines = utils::read_file(input_file).unwrap();
+    0
+}
