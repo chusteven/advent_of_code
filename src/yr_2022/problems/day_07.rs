b4a78fac1d314a1f3e01539632328f41b33d6bf6
Steven Chu <stevenc1@gmail.com>
2022-12-10 08:44:00 -0800

    Tmp

--- src/yr_2022/problems/day_07.rs
+++ src/yr_2022/problems/day_07.rs
@@ -5,7 +5,85 @@ use std::collections::{HashMap, HashSet};
 use crate::yr_2022::problems::utils;
 
 pub fn solution_2(filepath: &str) -> i32 {
-    let _lines = utils::read_file(filepath).unwrap();
+    let lines = utils::read_file(filepath).unwrap();
+    let mut is_listing: bool = false;
+    let cur_dir = &mut String::new();
+    let mut dirs: Vec<String> = vec![];
+    let mut filesystem: HashMap<String, RefCell<Path>> = HashMap::new();
+    let mut child_to_parent: HashMap<String, String> = HashMap::new();
+    let mut parent_to_child: HashMap<String, String> = HashMap::new();
+
+    for (_i, line) in lines.iter().enumerate() {
+        if line.starts_with("$ ") {
+            let raw_command = line[2..line.len()].trim();
+            if raw_command.starts_with("cd") {
+                let nav_path = &raw_command[2..raw_command.len()].trim();
+                if *nav_path == ".." {
+                    dirs.pop();
+                } else {
+                    dirs.push(nav_path.to_string());
+                    child_to_parent.insert(dirs.join("/"), cur_dir.to_string());
+                    parent_to_child.insert(cur_dir.to_string(), dirs.join("/"));
+                }
+                *cur_dir = dirs.join("/");
+                is_listing = false;
+            } else {
+                is_listing = true
+            }
+        }
+        if is_listing {
+            if line.contains("$ ls") {
+                continue;
+            }
+            let parts: Vec<&str> = line.split(' ').collect();
+            let first = parts[0];
+            if first.starts_with("dir") {
+                let raw_dir = parts[1].trim();
+                let mut dirs_clone = dirs.clone();
+                dirs_clone.push(raw_dir.to_string());
+                // FIXME: It only contains the directories seen so far
+                let dir = dirs_clone.join("/");
+                match filesystem.entry(cur_dir.to_string()) {
+                    Vacant(e) => {
+                        let mut set = HashSet::new();
+                        set.insert(dir);
+                        e.insert(RefCell::new(Path {
+                            total_size: 0,
+                            dirs: set,
+                        }));
+                    }
+                    Occupied(mut e) => {
+                        let mut p = e.get_mut().borrow_mut();
+                        let tmp_dirs = &mut p.dirs;
+                        tmp_dirs.insert(dir);
+                    }
+                }
+            } else {
+                // File size
+                let size = first.trim().to_string().parse::<i32>().unwrap();
+                match filesystem.entry(cur_dir.to_string()) {
+                    Vacant(e) => {
+                        e.insert(RefCell::new(Path {
+                            total_size: size,
+                            dirs: HashSet::new(),
+                        }));
+                    }
+                    Occupied(mut e) => {
+                        let mut p = e.get_mut().borrow_mut();
+                        p.total_size += size;
+                    }
+                }
+            }
+        }
+    }
+    let cur_dir = "/";
+    recurse(cur_dir, &filesystem, &parent_to_child);
+    let mut items = filesystem
+        .iter()
+        .map(|(k, v)| (k.to_string(), v.borrow().total_size))
+        .collect::<Vec<(String, i32)>>();
+    items.sort_by(|a, b| b.1.cmp(&a.1)); // Reverse
+    println!("{:?}", items);
     0
 }
 
