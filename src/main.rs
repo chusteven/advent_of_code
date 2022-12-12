c15e92b145c1caf52e50d5f017024fde56e1b5c9
Steven Chu <stevenc1@gmail.com>
2022-12-11 18:56:31 -0800

    Wtf is a supermodule

--- src/main.rs
+++ src/main.rs
@@ -32,8 +32,37 @@ fn invoke_solutions(
     }
 }
 
+fn invoke_solutions_i128(
+    year: &str,
+    day: &str,
+    fn1: impl Fn(&str) -> i128,
+    fn2: impl Fn(&str) -> i128,
+    debug_only: bool,
+) {
+    let play = format!("./src/yr_{year}/test_cases/day_{day}_play.txt");
+    let real = format!("./src/yr_{year}/test_cases/day_{day}_real.txt");
+    let ans1 = fn1(&play);
+    println!("ans1 is {:?}", ans1);
+
+    if !debug_only {
+        let now = Instant::now();
+        let ans1 = fn1(&real);
+        let elapsed = now.elapsed();
+        println!("ans1 is {:?}; time elapsed: {:?}", ans1, elapsed);
+    }
+
+    let ans2 = fn2(&play);
+    println!("ans2 is {:?}", ans2);
+    if !debug_only {
+        let now = Instant::now();
+        let ans2 = fn2(&real);
+        let elapsed = now.elapsed();
+        println!("ans2 is {:?}; time elapsed: {:?}", ans2, elapsed);
+    }
+}
+
 fn main() {
-    invoke_solutions(
+    invoke_solutions_i128(
         "2022",
         "11",
         yr_2022::problems::day_11::solution_1,
