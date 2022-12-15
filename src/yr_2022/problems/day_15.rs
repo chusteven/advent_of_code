fae4fce0a5249cbeeb8852b14333e2b0bf54fd3f
Steven Chu <stevenc1@gmail.com>
2022-12-15 08:01:41 -0800

    Bad performance

--- src/yr_2022/problems/day_15.rs
+++ src/yr_2022/problems/day_15.rs
@@ -9,12 +9,6 @@ const DIRECTIONS: &[(i32, i32); 4] = &[
     (0, -1), // Left
 ];
 
-#[derive(Debug)]
-struct State {
-    pos: (i32, i32),
-    steps: i32,
-}
-
 #[allow(dead_code)]
 pub fn solution_2(input_file: &str) -> i32 {
     let lines = utils::read_file(input_file).unwrap();
@@ -28,8 +22,6 @@ pub fn solution_2(input_file: &str) -> i32 {
 pub fn solution_1(input_file: &str) -> i32 {
     let lines = utils::read_file(input_file).unwrap();
     let (sensors, beacons) = parse_input(lines);
-    // println!("{:?}", sensors);
-    // println!("{:?}", beacons);
 
     let mut max_x = 0;
     let mut max_y = 0;
@@ -50,36 +42,36 @@ pub fn solution_1(input_file: &str) -> i32 {
         }
     });
 
-    let mut max_x = max_x as usize;
-    let mut max_y = max_y as usize;
-
-    // For testing only
-    // max_x = 2;
-    // max_y = 4;
-
     // We may go into -x territory as well as -y territory --
     // this means we should find that offset (for each axis)
     // and we that to index into the final grid
+    //
+    // Doubling to be conservative (probably too much...)
+    let max_x = max_x as usize;
+    let max_y = max_y as usize;
 
-    // Doubling to be conservative
-    let mut grid = vec![vec!['.'; 2 * max_x + 1]; 2 * max_y + 1];
+    // This is what's taking forever :(
+    println!("Filling -- ({max_y}, {max_y})");
+    // let mut grid =
+    //     vec![vec!['.'; ((max_x as f64 * 1.99) as usize) + 1]; ((max_y as f64 * 1.99) as usize) + 1];
+    let mut grid = vec![vec!['.'; max_x * 2 + 1]; max_y * 2 + 1];
+    println!("Filled");
 
-    // // For testing only
-    // // If I wanna put something at (0, 0)
-    // grid[max_y + 0][max_x + 0] = 'x';
-    // // If I wanna put something at (2, 0)
-    // grid[max_y + 0][max_x + max_x] = 'x';
-    // // If I wanna put something at (2, 4)
-    // grid[max_y + max_y][max_x + max_x] = 'x';
-    // println!("{:?}", grid);
+    println!("Marking");
+    for (s, b) in sensors.iter().zip(beacons.iter()) {
+        grid[(max_y as i32 + s.1) as usize][(max_x as i32 + s.0) as usize] = 'S';
+        grid[(max_y as i32 + b.1) as usize][(max_x as i32 + b.0) as usize] = 'B';
+    }
+    println!("Marked");
 
     for (s, b) in sensors.iter().zip(beacons.iter()) {
         mark_grid(s, b, &mut grid, &max_x, &max_y)
     }
-    let row_of_interest = 2_000_000;
+    let row_of_interest = if max_y > 2_000_000 { 2_000_000 } else { 10 };
     let mut ans = 0;
-    for i in 0..grid[row_of_interest].len() {
-        if grid[row_of_interest][i] == '#' {
+    for i in 0..grid[max_y + row_of_interest].len() {
+        let val = grid[max_y + row_of_interest][i];
+        if val == '#' || val == 'S' || val == 'B' {
             ans += 1;
         }
     }
@@ -95,35 +87,37 @@ fn mark_grid(
 ) {
     // BFS outward; stopping condition is when Manhattan distance is farther
     // than it is now
-
-    let mut queue: VecDeque<State> = VecDeque::new();
-    queue.push_back(State {
-        pos: *sensor,
-        steps: 0,
-    });
-    let start = *sensor;
+    let mut queue: VecDeque<(usize, usize)> = VecDeque::new();
+    queue.push_back((
+        (*max_x as i32 + sensor.0) as usize,
+        (*max_y as i32 + sensor.1) as usize,
+    ));
+    let start = (sensor.0 + *max_x as i32, sensor.1 + *max_y as i32);
     let manhattan_dist = (beacon.1 - sensor.1).abs() + (beacon.0 - sensor.0).abs();
     while !queue.is_empty() {
-        let node = queue.pop_front().unwrap();
-        let m = node.pos;
+        let m = queue.pop_front().unwrap();
         for dir in DIRECTIONS {
-            let next = (m.0 + dir.0, m.1 + dir.1);
-            if (max_y + next.1 as usize) >= grid.len() || (max_x + next.0 as usize) >= grid[0].len()
+            let next = (m.0 as i32 + dir.0, m.1 as i32 + dir.1);
+            if next.0 < 0
+                || next.1 < 0
+                || (next.1 as usize) >= grid.len()
+                || (next.0 as usize) >= grid[0].len()
             {
                 continue;
             }
-            let next_v = grid[max_y + next.0 as usize][max_x + next.1 as usize];
-            if next_v == 'S' || next_v == 'B' || next_v == '#' {
+            let next = (next.0 as usize, next.1 as usize);
+            let next_v = grid[next.1][next.0];
+            if next_v == 'S'
+                || next_v == 'B'
+                || next_v == '#'
+                || (next.1 as i32 - start.1).abs() + (next.0 as i32 - start.0).abs()
+                    > manhattan_dist
+            {
                 continue;
+            } else {
+                grid[next.1][next.0] = '#';
             }
-            if (next.1 - start.1).abs() + (next.0 - start.0).abs() > manhattan_dist {
-                continue;
-            }
-            grid[max_y + (next.1 as usize)][max_x + (next.0 as usize)] = '#';
-            queue.push_back(State {
-                pos: next,
-                steps: node.steps + 1,
-            });
+            queue.push_back(next);
         }
     }
 }
