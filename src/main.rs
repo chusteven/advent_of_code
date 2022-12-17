64b1c6a7cd0320b9145f9e90598b785eab913b4c
Steven Chu <stevenc1@gmail.com>
2022-12-17 09:41:27 -0800

    Start of day 16, but I do not really feel like doing more graph searching problems :D (#17)

--- src/main.rs
+++ src/main.rs
@@ -65,9 +65,9 @@ fn invoke_solutions_i64(
 fn main() {
     invoke_solutions(
         "2022",
-        "15",
-        yr_2022::problems::day_15::solution_1,
-        yr_2022::problems::day_15::solution_2,
-        false,
+        "16",
+        yr_2022::problems::day_16::solution_1,
+        yr_2022::problems::day_16::solution_2,
+        true,
     );
 }
